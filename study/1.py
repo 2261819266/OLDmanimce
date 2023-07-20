3588a5970415a7e761765fa44f469f6609b94236
yuxinhao <2261819266@qq.com>
2023-07-21 05:18:57 +0800

    master

--- study/1.py
+++ study/1.py
@@ -1,15 +1,15 @@
 from manim import *
 
-# class Begin(Scene) :
-#     def construct(self):
-#         print(FRAME_HEIGHT) # the height of the screen is 8 
-#         print(FRAME_WIDTH)  # FRAME_WIDTH = FRAME_HEIGHT * DEFAULT_PIXEL_WIDTH / DEFAULT_PIXEL_HEIGHT
-#         print(ORIGIN)       # ORIGIN => np.array([0, 0, 0])
+class Begin(Scene) :
+    def construct(self):
+        print(FRAME_HEIGHT) # the height of the screen is 8 
+        print(FRAME_WIDTH)  # FRAME_WIDTH = FRAME_HEIGHT * DEFAULT_PIXEL_WIDTH / DEFAULT_PIXEL_HEIGHT
+        print(ORIGIN)       # ORIGIN => np.array([0, 0, 0])
 
-#         print(RIGHT, UP, LEFT, DOWN)
-#         print(UR, UL, DR, DL)
-#         print(TOP, BOTTOM, RIGHT_SIDE, LEFT_SIDE)
-#         print(IN, OUT)
+        print(RIGHT, UP, LEFT, DOWN)
+        print(UR, UL, DR, DL)
+        print(TOP, BOTTOM, RIGHT_SIDE, LEFT_SIDE)
+        print(IN, OUT)
 
 
 # class Move(Scene) :
@@ -27,17 +27,17 @@ from manim import *
 #         self.play(mob.move_to, LEFT * 3, aligned_edge = LEFT) 
 #         self.play(mob.move_to, LEFT * 3, aligned_edge = RIGHT) 
 
-class Scale(Scene) :
-    def construct(self):
-        mob = Square()
-        # self.add(mob)
-        self.play(FadeIn(mob))
-        self.play(mob.scale, 2)
-        self.play(mob.scale, 0.5)
+# class Scale(Scene) :
+#     def construct(self):
+#         mob = Square()
+#         # self.add(mob)
+#         self.play(FadeIn(mob))
+#         self.play(mob.scale, 2)
+#         self.play(mob.scale, 0.5)
 
-        mob.scale(2, about_edge = UP)
-        # self.play(mob.scale, 2, about_edge = UP)
-        # self.play(mob.scale, 0.5, about_edge = DOWN)
-        self.play(mob.animate.scale(0.5, about_edge = DOWN))
-        self.play(mob.scale, 2, about_point = np.array([-2, -2, 0]))
+#         mob.scale(2, about_edge = UP)
+#         # self.play(mob.scale, 2, about_edge = UP)
+#         # self.play(mob.scale, 0.5, about_edge = DOWN)
+#         self.play(mob.animate.scale(0.5, about_edge = DOWN))
+#         self.play(mob.scale, 2, about_point = np.array([-2, -2, 0]))
 
